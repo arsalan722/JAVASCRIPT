9c7f7c9c85bc5389fb4d62959235cf77aafbe3e3
Arsalan Jamal <arsalanlaptop333@hotmail.com>
2024-05-02 22:44:02 +0500

    index.js

---
+++ JavaScript Conditionals- if, else if, else ladder/index.js
@@ -0,0 +1,26 @@
+console.log("Hey iam learning javascript")
+
+let age = 1;
+// let grace = 2;
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+if (age == 18) {
+    console.log("you can Go to gym");
+}
+
+else if (age == 0) {
+    console.log("are you kidding")
+}
+
+else {
+    console.log("you cannot go  gym ");
+}
+
+
+a = 6;
+b = 8;
+let = a > b ? (a - b): (b - a);
---
+++ JavaScript,Variables,Datatypes & Objects/index.js
@@ -0,0 +1,38 @@
+console.log("Hey this is arsalan learning javascript")
+
+let a = 5;
+a = a + 2
+let b = 5;
+let c = "arsalan";
+console.log(a)
+// console.log(a + b + 10)
+// var 2 = "zach"; Not Allowed
+// console.log(typeof a, typeof b, typeof c);
+
+
+// const x = 3;
+// const y = 6;
+// const z = 6;
+// console.log(x + y + z);
+// const a1 = 6;
+// a1 = a1 + 1; Not allowed because a1 is constant
+{
+    let a = 53;
+    console.log(a)
+}
+let p = 2;
+let q = "arsalan";
+let r = true;
+let s = null;
+let t = undefined;
+
+
+
+console.log(p,q,r,s,t)
+console.log(typeof p,typeof q,typeof r,typeof s,typeof t)
+
+let x = {
+ name: "arsalan",
+ "job code": 5600
+}
+console.log(x)
